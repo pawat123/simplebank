7fb8f1d62dd92d404de5ba1ce796a6b5a4052eab
pawat123 <pawat_boss@hotmail.com>
2022-08-04 13:51:14 +0700

    second commit

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,24 +1,44 @@
-
+-- name: CreateAccount :one
 INSERT INTO accounts (
-    owner,
-    balance,
-    currency
+  owner,
+  balance,
+  currency
 ) VALUES (
-    $1, $2, $3
-)RETURNING *;
+  $1, $2, $3
+) RETURNING *;
 
-SELECT * FROM accounts 
-WHERE id = $1 LIMIT 1; 
-
+-- name: GetAccount :one
 SELECT * FROM accounts
-ORDER BY id
-LIMIT $1
-OFFSET $2;
+WHERE id = $1 LIMIT 1;
 
-UPDATE accounts 
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+WHERE owner = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: UpdateAccount :one
+UPDATE accounts
 SET balance = $2
-WHERE id = $1 
+WHERE id = $1
 RETURNING *;
 
-DELETE  FROM accounts 
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
 WHERE id = $1;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
